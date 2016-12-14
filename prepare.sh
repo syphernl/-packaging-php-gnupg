fb2cb6563d286d0b082636ad71d99beb44c7a865
Frank Klaassen <frank.klaassen@enrise.com>
2016-12-14 09:35:14 +0100

    Install PHP7.0 by default on all platforms

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,12 @@ mkdir /src
 mkdir /dist
 mkdir packages
 
-apt-get update && apt-get install libgpgme11-dev -y
+PHP_VERSION=${PHP_VERSION:-7.0}
+
+# Install PHP repository
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+
+apt-get update && apt-get install php${PHP_VERSION}-dev libgpgme11-dev -y
 
 cd /src
 echo "[Prepare] Path: ${PWD}"
